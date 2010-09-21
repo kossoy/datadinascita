0b77049bd658227e79f1f8cefb1c77032b92a5bc
Oleg Kossoy <oleg@kossoy.com>
2010-09-21 18:34:28 +0200

    Exception handler

--- upload.py
+++ upload.py
@@ -13,24 +13,32 @@ from datetime import datetime
 
 class PhotoUploadHandler(blobstore_handlers.BlobstoreUploadHandler):
     def post(self):
-        upload_files = self.get_uploads('file')
-        blob_info = upload_files[0]
-        csv_file = fetch_data(blob_info, 0, blob_info.size + 1)
-        stringReader = csv.reader(StringIO(csv_file))
+        try:
+            upload_files = self.get_uploads('file')
+            blob_info = upload_files[0]
+            csv_file = fetch_data(blob_info, 0, blob_info.size + 1)
+            stringReader = csv.reader(StringIO(csv_file))
 
-        for row in stringReader:
-            logging.info(row)
+            for row in stringReader:
+                p = Person(name=row[1].decode('utf-8'))
+                p.owner = users.get_current_user()
+                p.birthday = datetime.date(datetime.strptime(row[0], "%m/%d/%Y"))
+                p.put()
 
-            p = Person(name=row[1].decode('utf-8'))
-            p.owner = users.get_current_user()
-            p.birthday = datetime.date(datetime.strptime(row[0], "%m/%d/%Y"))
-            p.put()
-
-        self.redirect('/')
+            self.redirect('/')
+        except:
+            self.redirect('/upload_failure/')
 
 class fail(webapp.RequestHandler):
     def get(self):
-        self.response.out.write('fail')
+        self.response.out.write('''
+            <html>
+            <body>
+                <h1 style="color: Red;">Failed</h1>
+                <a href="/import/">Try again</a>
+            <body>
+            <html>
+                                ''')
 
 application = webapp.WSGIApplication([
                                              ('/upload_csv/', PhotoUploadHandler),
